40a8cc10ecebd2b79ba76ee85b88bd9d0080f383
Josh Vote <jjvote@gmail.com>
2026-09-22 16:35:43 +1000

    Stubbed troca poll

--- src/cactus_juice/troca/poll.py
+++ src/cactus_juice/troca/poll.py
@@ -3,6 +3,8 @@ from datetime import UTC, datetime, timedelta
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from cactus_juice.crud import add_ocpp_readings
+from cactus_juice.csipaus.controls import calculate_schedule_values
 from cactus_juice.troca.client import TrocaClient
 from cactus_juice.troca.mapping import reading_to_db
 
@@ -22,6 +24,7 @@ class ClientState:
     connector_id: str  # The ID of the Troca connector that will be used for comms
     readings_poll: Pollable  # Poll status of the readings
     schedule_poll: Pollable  # Poll status of the charge schedule
+    metadata_poll: Pollable  # Poll status of the connected EVSE metadata
     ramp_step: timedelta
 
     def next_poll(self) -> datetime:
@@ -33,6 +36,7 @@ class ClientState:
         candidate_times = [
             _candidate_poll(self.readings_poll),
             _candidate_poll(self.schedule_poll),
+            _candidate_poll(self.metadata_poll),
         ]
 
         # The default should never occur - but just in case
@@ -44,6 +48,7 @@ class ClientState:
         connector_id: str,
         readings_poll_rate_seconds: int,
         schedule_poll_rate_seconds: int,
+        metadata_poll_rate_seconds: int,
         ramp_step_seconds: int,
     ) -> "ClientState":
         return ClientState(
@@ -51,6 +56,7 @@ class ClientState:
             connector_id=connector_id,
             readings_poll=Pollable(datetime.min, timedelta(seconds=readings_poll_rate_seconds)),
             schedule_poll=Pollable(datetime.min, timedelta(seconds=schedule_poll_rate_seconds)),
+            metadata_poll=Pollable(datetime.min, timedelta(seconds=metadata_poll_rate_seconds)),
             ramp_step=timedelta(seconds=ramp_step_seconds),
         )
 
@@ -58,11 +64,29 @@ class ClientState:
 async def poll_readings(state: ClientState, session: AsyncSession, now: datetime) -> None:
     """Takes a snapshot of OCPP readings via troca and pushes them into the DB"""
     readings = await state.client.get_metering_data()
-    session.add_all(reading_to_db(r) for r in readings)
+
+    await add_ocpp_readings(session, [reading_to_db(r) for r in readings])
     state.readings_poll.last_poll = now
 
 
-# async def poll_schedules(state: ClientState, session: AsyncSession, now: datetime) -> None:
+async def poll_schedules(state: ClientState, session: AsyncSession, now: datetime) -> None:
+    """Calculates the upcoming schedule of controls and maps it to a set of charging profiles. If those charging
+    profiles match the current troca profiles - this is a no-op, otherwise the existing set will be replaced/updated
+    with the newly calculated schedule"""
+    raw_schedule = await calculate_schedule_values(session, now)
+
+    raise NotImplementedError()
 
 
-# async def run_polls(state: ClientState) -> datetime:
+async def poll_metadata(state: ClientState, session: AsyncSession, now: datetime) -> None:
+    """Fetches metadata from the current charging session and updates the DB with the latest values"""
+    raise NotImplementedError()
+
+
+async def run_polls(state: ClientState) -> datetime:
+    """Runs every required poll of server, updating state as required. Will manage the creation of db sessions
+
+    Returns the next "wakeup" time for the next set of polls
+
+    Will not poll/post resources that have been polled recently"""
+    raise NotImplementedError()
